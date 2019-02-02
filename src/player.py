e692c708e00e08d9156664aefe3625ec91bbf7c2
Jason Tsang <jasonrtsang@gmail.com>
2019-02-01 16:05:03 -0800

    Initial commit

---
+++ src/player.py
@@ -0,0 +1,26 @@
+import items
+
+class Player:
+	def __init__(self):
+		self.inventory = [items.Rock(), items.Dagger(), 'Gold(5)',
+							'Crusty Bread']
+
+	def print_inventory(self):
+		print("Inventory:")
+		for item in self.inventory:
+			print('* ' + str(item))
+		best_weapon = self.most_powerful_weapon()
+		print("Your best weapon is your {}".format(best_weapon))
+
+	def most_powerful_weapon(self):
+		max_damage = 0
+		best_weapon = None
+		for item in self.inventory:
+			try:
+				if item.damage > max_damage:
+					max_damage = item.damage
+					best_weapon = item
+			except AttributeError:
+				pass
+
+		return best_weapon
